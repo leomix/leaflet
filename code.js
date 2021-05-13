302586dd6547c73f4d79a40bb3eebd527634e597
leonardo zavala <leozav@hotmail.com>
2021-05-13 14:58:42 -0500

    se agrega,interaccion con los divs en la parte inferior, se compacta la creacion de div para usar metodologia DRY

--- code.js
+++ code.js
@@ -23,19 +23,20 @@ let iconMarker = L.icon({
 
 //A +1DESHABILITA EL ZOOM EN DOBLE CLICK +2 PONER MARCADOR EN ESE PUNTO +4 PEDIR UN NOMBRE A LA ETIQUETA Y +5 TENER LA FUNCIONALIDAD DE CENTRARSE AL DARLE CLIK AL MARCADOR +6 AGREGANDOLO AL LISTADO DE MARCADORES
 // SE TRASLAPA CON D USAR UNO O OTRO
+
 myMap.doubleClickZoom.disable()
 
 myMap.on('dblclick', e => {
-    let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-    let nombre = prompt("Please enter your name")
-    marker = L.marker([latLng.lat, latLng.lng], { icon: iconMarker })
-        .bindPopup(nombre)
-        .addTo(myMap)
-        .on('click', centerMapOnPost)
-    markersById[Object.keys(markersById).length] = marker
+    const latLng = myMap.mouseEventToLatLng(e.originalEvent);
+    const nombre = prompt("Please enter your name")
+    const id = Object.keys(markersById).length
+    createMarker(latLng.lat, latLng.lng,nombre,id)
+    createDiv(id,nombre)
     
 })
+
 //B USA LA FUNCTION NATIVA DE LEAFLET PARA ENCONTRAR LA UBICACION DEL USUARIO Y MARCAR UN CIRCULO Y UN MARCADOR EN SU UBICACION
+
 myMap.locate({setView: true, 
     maxZoom: 16, 
     watch:true
@@ -49,7 +50,9 @@ L.circle(e.latlng, radius).addTo(myMap);
 }
 
 myMap.on('locationfound', onLocationFound);
+
 //C USA LA GEOLOCALIZACION NATIVA DEL EXPLORADOR PARA PONER UN MARCADOR ENCIMA DE TU UBICACION ACTUAL
+
 navigator.geolocation.getCurrentPosition(
     (pos) => {
         const { coords } = pos
@@ -69,39 +72,32 @@ navigator.geolocation.getCurrentPosition(
         maximumAge: 0
     })
 
-//D MUESTRA LA LATITUD Y LONGITUD AL DAR CLICK CON EL MOUSE SOBRE EL MAPA **AHORA TAMBIEN BUSCA LA DIRECCION
+//D MUESTRA LA LATITUD Y LONGITUD AL DAR CLICK CON EL MOUSE SOBRE EL MAPA **AHORA TAMBIEN BUSCA LA DIRECCION Y LA AÑADE EN UN DIV EN LA PARTE INFERIOR DEL MAPA
 // SE TRASLAPA CON A USAR UNO O OTRO
-async function onMapClick(e) {
-    const result = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${e.latlng.lat}&lon=${e.latlng.lng}&zoom=18&addressdetails=1`)
-    const json = await result.json()
-    console.log(json)
-    const nueva_dir = document.createElement('div')
-    nueva_dir.innerHTML = json.display_name
-    document.querySelector('.direcciones').appendChild(nueva_dir)
 
-}
+// async function onMapClick(e) {
+//     const result = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${e.latlng.lat}&lon=${e.latlng.lng}&zoom=18&addressdetails=1`)
+//     const json = await result.json()
+//     const nueva_dir = document.createElement('div')
+//     nueva_dir.innerHTML = json.display_name
+//     document.querySelector('.direcciones').appendChild(nueva_dir)
 
-function Get(whateverUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",whateverUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
-}
+// }
 
-myMap.on('click', onMapClick)
+// myMap.on('click', onMapClick)
 
 //E FUNCION QUE AGREGA LOS MARCADORES QUE SE OBTIENEN DEL ARREGLO DE DIRECCIONES DEL INICIO 
+
 for (var i = 0; i < locations.length; i++) {
-    marker = new L.marker([locations[i][1], locations[i][2]])
-        .bindPopup(locations[i][0])
-        .addTo(myMap)
-        .on('click', centerMapOnPost)
-        markersById[i]=marker
+    createDiv(i,locations[i][0])
+    createMarker(locations[i][1], locations[i][2],locations[i][0],i)
+
 }
 function centerMapOnPost() {
     myMap.panTo(this.getLatLng())
-    console.log(markersById)
 }
+
+//F AGREGA UN CONTROL PARA BUSCAR UNA DIRECCION Y APUNTAR EN ESA DIRECCION
 
 myMap.addControl( new L.Control.Search({
     url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}+Mexico',
@@ -113,3 +109,25 @@ myMap.addControl( new L.Control.Search({
     autoType: false,
     minLength: 2
 }) );
+
+//G FUNCION GENERAL PARA AGREGAR EL DIV DEBAJO DEL MAPA
+
+function createDiv(id,text){
+    const nueva_dir = document.createElement('div')
+    nueva_dir.innerHTML = text
+    nueva_dir.onclick = function(){
+        myMap.panTo(markersById[id].getLatLng())
+        markersById[id].fire('click')
+    };
+    document.querySelector('.direcciones').appendChild(nueva_dir)
+}
+
+//H FUNCION PARA GENERAR LOS MARCADORES
+
+function createMarker(lat,lng,text,i){
+    marker = new L.marker([lat, lng], { icon: iconMarker })
+        .bindPopup(text)
+        .addTo(myMap)
+        .on('click', centerMapOnPost)
+    markersById[i]=marker
+}
